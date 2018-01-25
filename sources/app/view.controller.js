9600cd5a12fd2ea393c21e5ec0938b165e8a27c3
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2018-01-26 00:54:57 +0100

    Upgraded version

--- sources/app/view.controller.js
+++ sources/app/view.controller.js
@@ -1,76 +1,110 @@
 (function () {
     'use strict';
 
-    angular.module('openVPNStatusMonitor').controller('ViewController', function ($scope, $timeout) {
-        var LZW = {
-            compress: function (uncompressed) {
-                var i,
-                    dictionaryBase = [],
-                    dictionary = {},
-                    c,
-                    wc,
-                    w = "",
-                    result = [],
-                    dictSize,
-                    baseDictSize;
-                dictionaryBase = uncompressed.split('').filter(function onlyUnique(value, index, self) {
-                    return self.indexOf(value) === index;
-                });
-                for (i = 0; i < dictionaryBase.length; i += 1) {
-                    dictionary[dictionaryBase[i]] = i;
-                }
-                baseDictSize = dictionaryBase.length;
-                dictSize = dictionaryBase.length;
-                for (i = 0; i < uncompressed.length; i += 1) {
+    angular.module('openVPNStatusMonitor').controller('ViewController', function ($scope) {
+        function LZWW(uncompressed) {
+            var i = 0,
+                dictionaryBase = [],
+                dictionary = {},
+                c,
+                wc,
+                w = "",
+                result = [],
+                dictSize = 0,
+                baseDictSize = 0;
+            this.step = function () {
+                if (i === 0 && dictSize === 0) {
+                    dictionaryBase = uncompressed.split('').filter(function onlyUnique(value, index, self) {
+                        return self.indexOf(value) === index;
+                    });
+                    for (var j = 0; j < dictionaryBase.length; j += 1) {
+                        dictionary[dictionaryBase[j]] = j;
+                    }
+                    baseDictSize = dictionaryBase.length;
+                    dictSize = dictionaryBase.length;
+                } else if (i < uncompressed.length) {
                     c = uncompressed.charAt(i);
                     wc = w + c;
                     if (dictionary.hasOwnProperty(wc)) {
                         w = wc;
                     } else {
                         result.push(dictionary[w]);
-                        // Add wc to the dictionary.
                         dictionary[wc] = dictSize++;
                         w = String(c);
                     }
-                }
-                if (w !== "") {
+                    i++;
+                } else if (i === uncompressed.length && w !== "") {
                     result.push(dictionary[w]);
+                    return null;
+                } else {
+                    return null;
                 }
                 return {
                     result:       result,
                     dictionary:   dictionary,
-                    baseDictSize: baseDictSize
+                    dictSize:     dictSize,
+                    baseDictSize: baseDictSize,
+                    step:         i
                 };
-            }
-        };
-        $scope.data = 'AABBBBAACCDDDDDDDCC';
-        $scope.genEntryStyle = function (entry) {
-            return {
+            };
+        }
+        $scope.getDictEntryStyle = function (entry) {
+            const style = {
                 display: 'inline-block',
                 width:   ((Math.floor(entry.value.length / 4) + 1) * 48) + 'px'
+            };
+            if ($scope.prevResult && entry.id >= $scope.prevResult.raw.dictSize) {
+                style['background-color'] = '#96D196';
             }
+            return style;
         };
-        $scope.compress = function () {
-            if ($scope.data && $scope.data.length > 0) {
-                var result = LZW.compress($scope.data);
-                var dictionary = [];
-                for (var key in result['dictionary']) {
-                    // noinspection JSUnfilteredForInLoop
-                    dictionary.push({
-                        id:    result['dictionary'][key],
-                        value: key
-                    })
+        $scope.getResultEntryStyle = function (index) {
+            const style = {
+                display: 'inline-block',
+                width:   '48px'
+            };
+            if ($scope.prevResult && (index + 1) >= $scope.prevResult.result.length) {
+                style['background-color'] = '#96D196';
+            }
+            return style;
+        };
+        $scope.makeStep = function () {
+            if ($scope.runnable && $scope.data && $scope.data.length > 0) {
+                if ($scope.lzw === null) {
+                    $scope.lzw = new LZWW($scope.data)
                 }
-                $scope.result = {
-                    result:     result.result,
-                    dictionary: {
-                        base:     dictionary.slice(0, result.baseDictSize),
-                        extended: dictionary.slice(result.baseDictSize)
+                var result = $scope.lzw.step();
+                if (result === null) {
+                    $scope.runnable = false;
+                } else {
+                    var dictionary = [];
+                    for (var key in result['dictionary']) {
+                        // noinspection JSUnfilteredForInLoop
+                        dictionary.push({
+                            id:    result['dictionary'][key],
+                            value: key
+                        })
                     }
-                };
-                console.log($scope.result);
+                    $scope.prevResult = $scope.result;
+                    $scope.result = {
+                        raw:        result,
+                        result:     result.result,
+                        dictionary: {
+                            base:     dictionary.slice(0, result.baseDictSize),
+                            extended: dictionary.slice(result.baseDictSize)
+                        }
+                    };
+                }
             }
         };
+        $scope.reset = function () {
+            $scope.lzw = null;
+            $scope.result = null;
+            $scope.prevResult = null;
+            $scope.runnable = true;
+        };
+        $scope.data = 'AABBBBAACCDDDDDDDCC';
+        $scope.reset();
     });
 
 })();
